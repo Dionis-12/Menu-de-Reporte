8820d8048d58e3fe23c21e9b5557eb6781a7b507
Dionis <djquijadateran@gmail.com>
2020-07-29 16:15:21 -0400

    Se agrega el index de la Página WEB

---
+++ index.php
@@ -0,0 +1,47 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1"/>
+	<link href="style.css" rel="stylesheet">
+	<link rel="icon" href="movistar.ico">
+	<title> Reportes Broadcast </title>
+</head>
+
+<body>
+	<!-- Título de la Página -->
+	<h1> Reportes Broadcast </h1>
+
+	<!-- Menú Tabs -->
+	<div class="pc-tab">
+		<input type="radio" name="pct" id="tab1" checked="checked">
+		<input type="radio" name="pct" id="tab2">
+		<nav>
+			<ul>
+				<li class="tab1">
+					<label for="tab1"><b> Broadcast Canales </b></label>
+				</li>
+				<li class="tab2">
+					<label for="tab2"><b> Broadcast Call </b></label>
+				</li>
+			</ul>
+		</nav>
+
+		<!-- Contenido por cada Tabs -->
+		<section>
+			<div class="tab1">
+				<h2><a href="http://localhost/Broadcast/Broadcast_Canales/"> Broadcast Canales </a></h2>
+			</div>
+			<div class="tab2">
+				<h2><a href="http://localhost/Broadcast/Broadcast_Call/"> Broadcast Call </a></h2>
+			</div>
+		</section>
+	</div>
+
+	<!-- Imagen Telefónica -->
+	<div class="tab-img">
+		<img src="telefonica-logos.png" title="Telefónica" alt="Imagenen description">
+	</div>
+
+</body>
+</html>
